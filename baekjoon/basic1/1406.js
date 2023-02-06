f9e01ae68b48ee9607eb9ef41deee5cf2bd90942
YOOJS1205 <junsang8921@gmail.com>
2023-02-06 13:59:14 +0900

    [0206] 1406번 코드 수정

--- baekjoon/basic1/1406.js
+++ baekjoon/basic1/1406.js
@@ -1,35 +1,10 @@
 // 에디터
+// 커서를 기준으로 두 개의 스택으로 나누자.
+// 그냥 배열로 쓰면 시간 초과뜬다...
+// 꼭 다시 풀어보기
 
 let fs = require('fs');
 let input = fs.readFileSync('../example.txt').toString().split('\n');
-
-// function solution(input) {
-//   let inputAry = input.map((item) => item.split(' '));
-//   let cursor = wordAry.length;
-//   for (const x of inputAry) {
-//     switch (x[0]) {
-//       case 'L':
-//         cursor && cursor--;
-//         break;
-//       case 'D':
-//         cursor !== wordAry.length && cursor++;
-//         break;
-//       case 'B':
-//         if (cursor) {
-//           wordAry.splice(cursor - 1, 1);
-//           cursor--;
-//         }
-//         break;
-//       case 'P':
-//         wordAry.splice(cursor, 0, x[1]);
-//         cursor++;
-//         break;
-//       default:
-//         break;
-//     }
-//   }
-//   console.log(wordAry.join(''));
-// }
 
 function solution(input) {
   let wordAry = input[0].split('');
